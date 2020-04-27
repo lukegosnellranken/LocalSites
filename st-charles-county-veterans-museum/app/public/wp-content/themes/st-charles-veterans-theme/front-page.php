393369be95df843213c0572b5724efb620669d0e
Luke Gosnell <luke_gosnell@ranken.edu>
2020-04-27 18:07:37 -0500

    4/27/2020 commit

--- st-charles-county-veterans-museum/app/public/wp-content/themes/st-charles-veterans-theme/front-page.php
+++ st-charles-county-veterans-museum/app/public/wp-content/themes/st-charles-veterans-theme/front-page.php
@@ -19,24 +19,8 @@
         <h2 class="headline headline--small-plus t-center">Latest Events</h2>
 
         <?php
-          $today = date('Ymd');
+          $today = 3000-01-01;
           $homepageEvents = new WP_Query(array(
-            'posts_per_page' => 2,
-            'post_type' => 'event',
-            'meta_key' => 'event_date',
-            'orderby' => 'meta_value_num',
-            'order' => 'ASC',
-            'meta_query' => array(
-              array(
-                'key' => 'event_date',
-                'compare' => '>=',
-                'value' => $today,
-                'type' => 'numeric'
-              )
-            )
-          ));
-
-          $homepageEventsOnlyPast = new WP_Query(array(
             'posts_per_page' => 2,
             'post_type' => 'event',
             'meta_key' => 'event_date',
@@ -45,7 +29,7 @@
             'meta_query' => array(
               array(
                 'key' => 'event_date',
-                'compare' => '<',
+                'compare' => '>=',
                 'value' => $today,
                 'type' => 'numeric'
               )
@@ -71,33 +55,7 @@
                     } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
               </div>
             </div>
-          <?php }
-
-          // if no future events, show 2 most recent past events in DESC order
-
-          if(!$homepageEvents->have_posts()){
-            while($homepageEventsOnlyPast->have_posts()) {
-              $homepageEventsOnlyPast->the_post(); ?>
-            <div class="event-summary">
-              <a class="event-summary__date t-center" href="#">
-                <span class="event-summary__month"><?php
-                  $eventDate = DateTime::createFromFormat('d/m/Y',  get_field( 'event_date' ));
-                  echo $eventDate->format('M')
-                ?></span>
-                <span class="event-summary__day"><?php echo $eventDate->format('d') ?></span>  
-              </a>
-              <div class="event-summary__content">
-                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                <p><?php if (has_excerpt()) {
-                    echo get_the_excerpt();
-                  } else {
-                    echo wp_trim_words(get_the_content(), 18);
-                    } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-              </div>
-            </div>
-          <?php }
-          }
-            
+            <?php }
         ?>
 
 
