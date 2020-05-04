32170e943e91976e8303d8e13fca9947bceaa141
Luke Gosnell <luke_gosnell@ranken.edu>
2020-05-03 21:19:36 -0500

    5/3/2020 commit 2

--- st-charles-county-veterans-museum/app/public/wp-content/themes/st-charles-veterans-theme/header.php
+++ st-charles-county-veterans-museum/app/public/wp-content/themes/st-charles-veterans-theme/header.php
@@ -15,17 +15,17 @@
             <div class="site-header__menu group">
             <nav class="main-navigation">
                 <ul>
-                <li <?php if (is_page('news') OR get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/news') ?>">News</a></li>
-                <li <?php if (is_page('events') OR get_post_type() == 'event' OR is_page('past-events')) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/events') ?>">Events</a></li>
-                <li <?php if (is_page('stories') OR get_post_type() == 'story' OR get_post_type() == 'war') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/stories') ?>">Stories</a></li>
-                <li <?php if (is_page('videos')) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/videos') ?>">Videos</a></li>
-                <li <?php if (is_page('gallery')) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/gallery') ?>">Gallery</a></li>
-                <li <?php if (is_page('sponsors') or wp_get_post_parent_id(0)) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/sponsors') ?>">Sponsors</a></li>
-                <li <?php if (is_page('volunteers')) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/volunteers') ?>">Volunteers</a></li>
+                <li class="nav-item" <?php if (is_page('news') OR get_post_type() == 'post') echo 'id="current-menu-item"' ?>><a href="<?php echo site_url('/news') ?>">News</a></li>
+                <li class="nav-item" <?php if (is_page('events') OR get_post_type() == 'event' OR is_page('past-events')) echo 'id="current-menu-item"' ?>><a href="<?php echo site_url('/events') ?>">Events</a></li>
+                <li class="nav-item" <?php if (is_page('stories') OR get_post_type() == 'story' OR get_post_type() == 'war') echo 'id="current-menu-item"' ?>><a href="<?php echo site_url('/stories') ?>">Stories</a></li>
+                <li class="nav-item" <?php if (is_page('videos')) echo 'id="current-menu-item"' ?>><a href="<?php echo site_url('/videos') ?>">Videos</a></li>
+                <li class="nav-item" <?php if (is_page('gallery')) echo 'id="current-menu-item"' ?>><a href="<?php echo site_url('/gallery') ?>">Gallery</a></li>
+                <li class="nav-item" <?php if (is_page('sponsors') or wp_get_post_parent_id(0)) echo 'id="current-menu-item"' ?>><a href="<?php echo site_url('/sponsors') ?>">Sponsors</a></li>
+                <li class="nav-item" <?php if (is_page('volunteers')) echo 'id="current-menu-item"' ?>><a href="<?php echo site_url('/volunteers') ?>">Volunteers</a></li>
                 </ul>
             </nav>
             <div class="site-header__util">
-                <a href="<?php echo site_url('/donate') ?>" class="btn btn--small btn--orange float-left push-right">Donate</a>
+                <a href="<?php echo site_url('/donate') ?>" class="btn btn--small btn--orange float-left push-right" id="donate-button-nav">Donate</a>
                 
                 <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
             </div>
--- st-charles-county-veterans-museum/app/public/wp-content/themes/st-charles-veterans-theme/js/scripts.js
+++ st-charles-county-veterans-museum/app/public/wp-content/themes/st-charles-veterans-theme/js/scripts.js
@@ -10,3 +10,4 @@ import Search from './modules/Search';
 // Instantiate a new object using our modules/classes
 var mobileMenu = new MobileMenu();
 var heroSlider = new HeroSlider();
+var search = new Search();
